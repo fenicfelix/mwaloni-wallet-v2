cfe9d591cb3988f1271a4a8ca84a89a46d5f0fd1
Felix Ogucha <fenicfelix@gmail.com>
2026-01-24 00:54:27 +0000

    Updates horizon

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -29,7 +29,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user = null) {
             return in_array(optional($user)->email, [
-                //
+                'akika.digital@gmail.com',
+                'osanjo@gmail.com'
             ]);
         });
     }
