7f7ae11154a74708e172a44a65398edb0989c453
Felix Ogucha <fenicfelix@gmail.com>
2026-01-28 11:12:11 +0000

    Updates error message

--- packages/Wallet/Core/src/Jobs/Daraja/ProcessDarajaB2CPayment.php
+++ packages/Wallet/Core/src/Jobs/Daraja/ProcessDarajaB2CPayment.php
@@ -43,7 +43,7 @@ class ProcessDarajaB2CPayment implements ShouldQueue
         }
 
         $response = $this->performTransaction($transaction->identifier, "BusinessPayment", $transaction->account_number, floor($transaction->disbursed_amount), $transaction->description, NULL, $transaction->account);
-        info('PAYMENT_RESPONSE: '.json_encode($response));
+        info('PAYMENT_RESPONSE: ' . json_encode($response));
         if ($response) {
             $updateData = [];
             $payloadData = [
@@ -56,10 +56,14 @@ class ProcessDarajaB2CPayment implements ShouldQueue
                     "result_description" => $response->ResponseDescription
                 ];
 
+                info('PAYMENT_UPDATE: ' . json_encode($updateData));
+
                 $payloadData = [
                     "conversation_id" => $response->ConversationID,
                     "original_conversation_id" => $response->OriginatorConversationID
                 ];
+
+                info('PAYMENT_PAYLOAD: ' . json_encode($payloadData));
             } catch (\Throwable $th) {
                 $updateData = [
                     "status" => TransactionStatus::FAILED,
