17f94677654a84f94ef950e10f0d58c435198342
Felix Ogucha <fenicfelix@gmail.com>
2026-03-05 13:28:07 +0300

    Updates momo facade

--- packages/Wallet/Core/src/Jobs/Momo/QueryMomoBalance.php
+++ packages/Wallet/Core/src/Jobs/Momo/QueryMomoBalance.php
@@ -39,7 +39,7 @@ class QueryMomoBalance implements ShouldQueue
         }
 
         $balanceData = $this->fetchBalance($account);
-        info('BALANCE_DATA: ' . json_encode($balanceData));
+        info('BALANCE_DATA: ' . $balanceData['availableBalance']);
         $account->utility_balance = $balanceData['availableBalance'] ?? 0;
         $account->save();
     }
